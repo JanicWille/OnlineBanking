4fc943542d5de8eed0dabc6e93126de4bc563a43
Janic Wille <janic.wille@dataport.de>
2020-05-28 14:18:37 +0200

    Den Details wurde eine Zeile für das Anzeigen der IBAN und des Kontotyps hinzugefügt

--- OnlineBanking/dal/BankingContext.cs
+++ OnlineBanking/dal/BankingContext.cs
@@ -16,7 +16,7 @@ namespace OnlineBanking.DAL
         public DbSet<Konto> Konto { get; set; }
         public DbSet<KontoTyp> KontoTyp { get; set; }
         public DbSet<Kunde> Kunde { get; set; }
-        //public DbSet<KundeKonto> KundeKonto { get; set; }
+        public DbSet<KundeKonto> KundeKonto { get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
